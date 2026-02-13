ffb3c4079e5381797e2f515102f46b3b576d3525
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-13 12:46:05 +0000

    Handle errors inside authorController

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,14 +3,19 @@ const db = require("../db");
 async function getAuthorById(req, res) {
     const { authorId } = req.params;
 
-    const author = await db.getAuthorById(+authorId);
+    try {
+        const author = await db.getAuthorById(+authorId);
 
-    if (!author) {
-        res.status(404).send("Author not found");
-        return;
+        if (!author) {
+            res.status(404).send("Author not found");
+            return;
+        }
+
+        res.send(`Author name: ${author.name}`);
+    } catch (error) {
+        console.log("Error retrieving author :", error);
+        res.status(500).send("Internal Server Error");
     }
-
-    res.send(`Author name: ${author.name}`);
 }
 
 module.exports = { getAuthorById };
